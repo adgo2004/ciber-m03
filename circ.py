2f4965d74f734def15e61d8521ac6c37a6e8a519
adgo2004 <isard@debian>
2024-12-09 18:34:40 +0100

    segon commit

--- circ.py
+++ circ.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
- 
+
 # Arxiu circ.py : càlcul perímetre d'una circumferència
- 
+# Autor: Adrián Gordillo
+
 radi = input("Radi de la circumferència? ")
 PI = 3.14
 perimetre = 2*PI*float(radi)
